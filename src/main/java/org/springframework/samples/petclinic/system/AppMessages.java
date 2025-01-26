6af8ab7cac64386c46dd06066fd951a2dd236528
Antoine Rey <antoine.rey@free.fr>
2025-01-26 22:09:56 +0100

    Quarkus migration: i18n

---
+++ src/main/java/org/springframework/samples/petclinic/system/AppMessages.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.petclinic.system;
+
+import io.quarkus.qute.i18n.Message;
+import io.quarkus.qute.i18n.MessageBundle;
+
+@MessageBundle(value = "messages", locale = "en")
+public interface AppMessages {
+
+    @Message
+    String welcome();
+
+	@Message
+	String required();
+
+	@Message
+	String notFound();
+
+	@Message
+	String duplicate();
+
+	@Message
+	String nonNumeric();
+
+	@Message
+	String duplicateFormSubmission();
+
+	@Message
+	String typeMismatch_date();
+
+	@Message
+	String typeMismatch_birthDate();
+
+}
