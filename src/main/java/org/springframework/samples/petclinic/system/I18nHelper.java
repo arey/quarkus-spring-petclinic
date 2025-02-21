2a91ce6605af579a3a17de6180a9689d88184048
Antoine Rey <antoine.rey@free.fr>
2025-02-21 18:48:51 +0100

    Quarkus migration: add the I18nHelper class

---
+++ src/main/java/org/springframework/samples/petclinic/system/I18nHelper.java
@@ -0,0 +1,32 @@
+package org.springframework.samples.petclinic.system;
+
+import io.quarkus.qute.i18n.Localized;
+import io.quarkus.qute.i18n.MessageBundles;
+
+import java.util.List;
+import java.util.Locale;
+
+public class I18nHelper {
+
+	private I18nHelper() {
+		// Helper class
+	}
+
+	public static AppMessages lookupAppMessages(String acceptLanguage) {
+		List<Locale.LanguageRange> languageRanges = Locale.LanguageRange.parse(acceptLanguage);
+		Locale locale = Locale.lookup(languageRanges, List.of(
+			Locale.forLanguageTag("en"),
+			Locale.forLanguageTag("de"),
+			Locale.forLanguageTag("fa"),
+			Locale.forLanguageTag("ko"),
+			Locale.forLanguageTag("pt"),
+			Locale.forLanguageTag("ru"),
+			Locale.forLanguageTag("tr")
+		));
+		if (locale == null) {
+			locale = Locale.forLanguageTag("en");
+		}
+		return MessageBundles.get(AppMessages.class, Localized.Literal.of(locale.toLanguageTag()));
+	}
+
+}
