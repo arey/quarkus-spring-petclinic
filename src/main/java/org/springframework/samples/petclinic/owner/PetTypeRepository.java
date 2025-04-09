210f02e20e48f8618fcfe37e9e12d6c4f1bbbfc4
Antoine Rey <antoine.rey@free.fr>
2025-04-09 16:53:59 +0200

    Quarkus migration: remove commented code

--- src/main/java/org/springframework/samples/petclinic/owner/PetTypeRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetTypeRepository.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.owner;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,12 +11,5 @@ public interface PetTypeRepository extends JpaRepository<PetType, Integer> {
 	 * @return a Collection of {@link PetType}s.
 	 */
 	List<PetType> findAllByOrderByName();
-
-	/**
-	 * Retrieve all {@link PetType}s from the data store.
-	 * @return a Collection of {@link PetType}s.
-	 */
-	// FIXME : à tester @Query("SELECT ptype FROM PetType ptype ORDER BY ptype.name")
-	// List<PetType> findPetTypes();
 
 }
